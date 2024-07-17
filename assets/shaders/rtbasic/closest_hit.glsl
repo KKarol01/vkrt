83a052d84d224b5412fa02a99f6851ec4e8904f9
ThinCan <01169581@pw.edu.pl>
2024-07-18 01:59:25 +0200

    per triangle buffer

--- assets/shaders/rtbasic/closest_hit.glsl
+++ assets/shaders/rtbasic/closest_hit.glsl
@@ -6,7 +6,7 @@
 #extension GL_EXT_shader_explicit_arithmetic_types_int32 : enable
 #extension GL_EXT_shader_explicit_arithmetic_types_int64 : enable
 
-layout(binding = 3, set = 0) uniform sampler2D textures[];
+layout(binding = 4, set = 0) uniform sampler2D textures[];
 
 struct Vertex {
     vec3 pos;
@@ -35,8 +35,6 @@ hitAttributeEXT vec2 attribs;
 
 void main()
 {
-  hitValue = vec3(gl_RayTmaxEXT / 5.0);
-  return;
   const vec3 barycentricCoords = vec3(1.0f - attribs.x - attribs.y, attribs.x, attribs.y);
   const Vertex verts[3] = Vertex[](
     vertex_buffer.vertices[gl_PrimitiveID + 0],
@@ -44,8 +42,7 @@ void main()
     vertex_buffer.vertices[gl_PrimitiveID + 2]
   );
   const vec2 uvs = verts[0].uv * attribs.x + verts[1].uv * attribs.y + verts[2].uv * barycentricCoords.x;
-  const float t = exp2(5.0 / float(gl_RayTmaxEXT));
-  const vec3 color_value = texture(textures[nonuniformEXT(triangle_materials.ids[gl_PrimitiveID])], uvs).rgb * (vec3(uvs.xy, 0.3)*0.5+0.5);
+  const vec3 color_value = texture(textures[nonuniformEXT(triangle_materials.ids[gl_PrimitiveID])], uvs).rgb;
   //const vec3 color_value = vec3(uvs.xy * 0.5 + 0.5, 0.0);
-  hitValue = pow(color_value * t, vec3(2.2));
+  hitValue = pow(color_value, vec3(gl_RayTmaxEXT));
 }
