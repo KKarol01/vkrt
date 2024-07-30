bc308bd24fde3c672bb43eda026cf6947f35dcda
KKarol01 <kubicki501@gmail.com>
2024-07-30 11:19:56 +0200

    halton jitter

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include <glm/gtx/euler_angles.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
-#include <random>
 #include "vulkan_structs.hpp"
 #include "model_importer.hpp"
 #include "engine.hpp"
@@ -23,6 +22,21 @@ struct Camera {
 };
 
 static Camera camera;
+
+// https://www.shadertoy.com/view/WlSSWc
+float halton(int i, int b) {
+    /* Creates a halton sequence of values between 0 and 1.
+        https://en.wikipedia.org/wiki/Halton_sequence
+        Used for jittering based on a constant set of 2D points. */
+    float f = 1.0;
+    float r = 0.0;
+    while(i > 0) {
+        f = f / float(b);
+        r = r + f * float(i % b);
+        i = i / b;
+    }
+    return r;
+}
 
 int main() {
     try {
@@ -50,13 +64,14 @@ int main() {
         memcpy(vk_renderer->ubo.mapped, &inv_view[0][0], sizeof(inv_view));
         memcpy(static_cast<std::byte*>(vk_renderer->ubo.mapped) + sizeof(inv_view), &inv_projection[0][0], sizeof(inv_projection));
 
-        std::mt19937 eng;
-        std::uniform_real_distribution<float> dist{ -0.01f, 0.01f };
+        int num_frame = 0;
 
         while(!glfwWindowShouldClose(Engine::window()->window)) {
-            glm::mat3 rand_mat = glm::rotate(glm::mat4{ 1.0f }, dist(eng), glm::vec3{ 1.0f, 0.0f, 0.0f }) *
-                                 glm::rotate(glm::mat4{ 1.0f }, dist(eng), glm::vec3{ 0.0f, 1.0f, 0.0f }) *
-                                 glm::rotate(glm::mat4{ 1.0f }, dist(eng), glm::vec3{ 0.0f, 0.0f, 1.0f });
+            float hx = (halton(num_frame, 2) * 2.0 - 1.0) / float(window->size[0]);
+            float hy = (halton(num_frame, 3) * 2.0 - 1.0) / float(window->size[1]);
+            num_frame = (num_frame + 1) % 4;
+            glm::mat3 rand_mat = glm::rotate(glm::mat4{ 1.0f }, hy, glm::vec3{ 1.0f, 0.0f, 0.0f }) *
+                                 glm::rotate(glm::mat4{ 1.0f }, hx, glm::vec3{ 0.0f, 1.0f, 0.0f });
             memcpy((glm::mat4*)vk_renderer->ubo.mapped + 2, &rand_mat[0][0], sizeof(rand_mat));
 
             vk_renderer->render();
