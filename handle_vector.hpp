8b2e63ec636176540408b32b3db1ed148c8f24d5
Karol01 <kubicki501@gmail.com>
2024-08-09 16:03:27 +0200

    instance transform buffer

--- handle_vector.hpp
+++ handle_vector.hpp
@@ -8,7 +8,6 @@
 
 template <typename T> class HandleVector {
     using Storage = uint32_t;
-    using Offset = int32_t;
 
     struct Iterator : public std::iterator<std::forward_iterator_tag, T> {
         using Ref = T&;
@@ -36,43 +35,40 @@ template <typename T> class HandleVector {
     };
 
   public:
-    auto begin() { return Iterator{ data.data() }; }
-    auto end() { return Iterator{ data.data() + data.size() }; }
+    constexpr auto begin() { return Iterator{ data.data() }; }
+    constexpr auto end() { return Iterator{ data.data() + data.size() }; }
 
-    Handle<T, Storage> push_back(const T& t) {
-        Handle<T, Storage> handle{ static_cast<Storage>(data.size()) };
-        data.emplace_back(handle, t);
-        offsets.push_back(0);
-        return handle;
-    }
-
-    Handle<T, Storage> push_back(T&& t) {
-        Handle<T, Storage> handle{ static_cast<Storage>(data.size()) };
-        data.emplace_back(handle, std::move(t));
-        offsets.push_back(0);
-        return handle;
+    template <class Data> constexpr Handle<T, Storage> push_back(Data&& d) {
+        Handle<T, Storage> h = gen_handle();
+        data.emplace_back(h, std::forward<Data>(d));
+        data_indices[h] = static_cast<Storage>(data.size() - 1ull);
+        return h;
     }
 
     T& get(Handle<T> h) { return data.at(index_of(h)).second; }
+    const T& get(Handle<T> h) const { return data.at(index_of(h)).second; }
 
-    constexpr Offset index_of(Handle<T> h) const { return *h + offsets.at(*h); }
+    constexpr Storage index_of(Handle<T, Storage> h) const { return data_indices.at(h); }
 
     constexpr T& at(Storage idx) { return data.at(idx).second; }
     constexpr const T& at(Storage idx) const { return data.at(idx).second; }
     constexpr bool empty() const { return data.empty(); }
     constexpr auto size() const { return data.size(); }
-    template<class Self> constexpr auto& front(this Self& self) { return self.data.front().second; }
-    template<class Self> constexpr auto& back(this Self& self) { return self.data.back().second; }
+    template <class Self> constexpr auto& front(this Self& self) { return self.data.front().second; }
+    template <class Self> constexpr auto& back(this Self& self) { return self.data.back().second; }
 
     template <typename SortFunc = std::less<>> void sort(SortFunc&& func = {}) {
         std::sort(data.begin(), data.end(), [&func](const auto& a, const auto& b) { return func(a.second, b.second); });
-        for(Offset i = 0; i < data.size(); ++i) {
+        for(Storage i = 0; i < data.size(); ++i) {
             const auto& [handle, elem] = data.at(i);
-            offsets.at(*handle) = *handle - i;
+            data_indices[handle] = i;
         }
     }
 
   private:
+    Handle<T, Storage> gen_handle() { return Handle<T, Storage>{ _handle++ }; }
+
+    static inline Storage _handle{};
     std::vector<std::pair<Handle<T>, T>> data;
-    std::vector<Offset> offsets;
+    std::unordered_map<Handle<T>, Storage> data_indices;
 };
