9949bfa8814e5ad52e98589d2edf58483ada7980
Karol01 <kubicki501@gmail.com>
2024-10-09 18:01:05 +0200

    imguizmo (not working)

--- ui.cpp
+++ ui.cpp
@@ -1,10 +1,15 @@
 #include "engine.hpp"
 #define IMGUI_DEFINE_MATH_OPERATORS
+#include <array>
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_vulkan.h>
+#include <ImGuizmo/ImGuizmo.h>
+#include <glm/gtc/matrix_transform.hpp>
 #include "ui.hpp"
 #include "renderer_vulkan.hpp"
+
+static std::array<std::pair<VkImageView, VkDescriptorSet>, 2> output_images;
 
 void UI::update() {
     auto renderer = ((RendererVulkan*)Engine::renderer());
@@ -45,23 +50,44 @@ void UI::update() {
             auto cpos = ImGui::GetCursorScreenPos();
             const ImVec2 image_output_size{ ImGui::GetContentRegionAvail().x + cell_padding.x,
                                             ImGui::GetContentRegionAvail().y * 0.5f };
-            if(ImGui::BeginChild("image output", image_output_size, ImGuiChildFlags_Border | ImGuiChildFlags_ResizeY)) {}
-            ImGui::EndChild();
-            {
-                static constexpr float aspect = 16.0f / 9.0f;
-                const float orig_width = image_output_size.x - ImGui::GetStyle().ItemSpacing.x * 0.5f + 1.0f;
-                const float orig_height = ImGui::GetCursorScreenPos().y - cpos.y - ImGui::GetStyle().ItemSpacing.y - 5.0f;
-                float width = orig_width;
-                float height = orig_height;
-                if(width > height * aspect) {
-                    width = height * aspect;
-                } else {
-                    height = width / aspect;
+
+            if(ImGui::BeginChild("image output", image_output_size, ImGuiChildFlags_Border | ImGuiChildFlags_ResizeY)) {
+                const auto fn = get_renderer().get_resource_idx();
+                const Image* oi = &get_renderer().output_images[fn];
+                if(oi && oi->view && output_images[fn].first != oi->view) {
+                    const auto linear_sampler = get_renderer().samplers.get_sampler();
+                    if(output_images[fn].second) {
+                        vkDeviceWaitIdle(get_renderer().dev);
+                        ImGui_ImplVulkan_RemoveTexture(output_images[fn].second);
+                    }
+                    output_images[fn].first = oi->view;
+                    output_images[fn].second =
+                        ImGui_ImplVulkan_AddTexture(linear_sampler, oi->view, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
                 }
-                float offsetX = cpos.x + 2.0f + std::abs(orig_width - width) * 0.5f;
-                float offsetY = cpos.y + ImGui::GetStyle().ItemSpacing.y + std::abs(orig_height - height) * 0.5f;
-                Engine::renderer()->set_screen_rect({ (int)offsetX, (int)offsetY, (uint32_t)width, (uint32_t)height });
+                if(oi && oi->view) {
+                    ImGui::Image((ImTextureID)output_images[fn].second, ImGui::GetContentRegionAvail());
+                }
             }
+            ImGui::EndChild();
+
+            static constexpr float aspect = 16.0f / 9.0f;
+            const float orig_width = image_output_size.x - ImGui::GetStyle().ItemSpacing.x * 0.5f + 1.0f;
+            const float orig_height = ImGui::GetCursorScreenPos().y - cpos.y - ImGui::GetStyle().ItemSpacing.y - 5.0f;
+            float width = orig_width;
+            float height = orig_height;
+            if(width > height * aspect) {
+                width = height * aspect;
+            } else {
+                height = width / aspect;
+            }
+            float offsetX = cpos.x + 2.0f + std::abs(orig_width - width) * 0.5f;
+            float offsetY = cpos.y + ImGui::GetStyle().ItemSpacing.y + std::abs(orig_height - height) * 0.5f;
+            output_offset_x = offsetX;
+            output_offset_y = offsetY;
+            output_offset_w = width;
+            output_offset_h = height;
+            Engine::renderer()->set_screen_rect({ (int)offsetX, (int)offsetY, (uint32_t)width, (uint32_t)height });
+
             ImGui::SetCursorPosX(ImGui::GetCursorPosX() - cell_padding.x);
             if(ImGui::BeginChild("log output", ImVec2{ ImGui::GetContentRegionAvail().x + cell_padding.x,
                                                        ImGui::GetContentRegionAvail().y + cell_padding.y })) {
@@ -157,6 +183,19 @@ void UI::draw_right_column() {
         Engine::renderer()->update_transform(data->renderer_handle);
     }
     ImGui::PopID();
+
+    ScreenRect srect{ .offset_x = (int)output_offset_x,
+                      .offset_y = (int)output_offset_y,
+                      .width = (uint32_t)output_offset_w,
+                      .height = (uint32_t)output_offset_h };
+    ImGuizmo::SetDrawlist(ImGui::GetForegroundDrawList());
+    ImGuizmo::SetRect(srect.offset_x, srect.offset_y, srect.width, srect.height);
+    const auto viewmat = Engine::camera()->get_view();
+    const auto projmat = Engine::camera()->get_projection();
+    static ImGuizmo::OPERATION op = ImGuizmo::OPERATION::TRANSLATE;
+    static ImGuizmo::MODE mode = ImGuizmo::MODE::WORLD;
+    static glm::mat4 tt = glm::translate(glm::mat4{ 1.0f }, glm::vec3{ -0.194f, -0.280f - 0.054f, 0.362f - 0.054f});
+    ImGuizmo::Manipulate(&viewmat[0][0], &projmat[0][0], op, mode, &tt[0][0]);
 }
 
 void UI::set_selected(Scene::MeshInstance* ptr) {
