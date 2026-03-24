a03b74c7a4d4dc05f6354d59a5f6769ec237ea09
Karol01 <kubicki501@gmail.com>
2026-03-24 19:46:16 +0100

    reverted shader

--- eng/renderer/shaders/imgui/imgui.ps.hlsl
+++ eng/renderer/shaders/imgui/imgui.ps.hlsl
@@ -2,5 +2,5 @@
 
 float4 main(VS_OUT input) : SV_Target0
 {
-    return input.color * gTexture2Ds[pc.color_tex].Sample(gSamplerStates[ENG_SAMPLER_LINEAR], input.uv) * float4(1.0, 1.0, 1.0, 1.0);
+    return input.color * gTexture2Ds[pc.color_tex].Sample(gSamplerStates[ENG_SAMPLER_LINEAR], input.uv);
 }
